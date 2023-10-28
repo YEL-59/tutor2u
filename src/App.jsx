941e691a7f211b0239620099a5a796f5d04e5684
Md. Tofayel Islam <tofayelislam19986@gmail.com>
2023-10-28 16:38:28 +0600

    create 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route,Navigate } from 'react-router-dom';
 import Home from './Components/Pages/Home/Home';
 import AboutUs from './Components/Pages/AboutUs/AboutUs';
 import ContactUs from './Components/Pages/ContactUs/ContactUs';
 import RequestTutor from './Components/Pages/RequestTutor/RequestTutor';
 import FindTutor from './Components/Pages/FindTutor/FindTutor';
+import NotFoundpage from './Components/Pages/NotFoundPage/NotFoundpage';
 const App = () => {
   return (
     <>
@@ -14,7 +15,8 @@ const App = () => {
         <Route path="/request_tutor" element={<RequestTutor/>} />
         <Route path="/find_tutor" element={<FindTutor/>}/>
 {/*  <Route path="/find_tutor" element={<FindTutor/>}/> */}
-
+<Route path="/404" element={<NotFoundpage />} />
+<Route path="*" element={<Navigate to="/404" />} />
       </Routes>
 
 
