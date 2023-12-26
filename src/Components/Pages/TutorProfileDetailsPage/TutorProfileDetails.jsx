d811830e36052117617b3b7ba688dbc1df5324e4
Md. Tofayel Islam <tofayelislam19986@gmail.com>
2023-12-26 23:46:02 +0600

    (update):add exploer tutor card with show more button feature

--- src/Components/Pages/TutorProfileDetailsPage/TutorProfileDetails.jsx
+++ src/Components/Pages/TutorProfileDetailsPage/TutorProfileDetails.jsx
@@ -5,8 +5,12 @@ import { useEffect, useState } from "react";
 import B from "../../SharedComponents/Data/Tutotprofiledata/TutorProfiledata.json";
 import Navbars from "../../SharedComponents/Navbar/Navbars";
 import TutorProfile from "../Props/TutorProfile/TutorProfile";
+import Footer from "../../SharedComponents/Footer/Footer";
+import ExploreTutordata from '../../SharedComponents/Data/ExploreTutordata/ExploreTutordata.json'
 
-const TutorProfileDetails = ({ tutorId }) => {
+import ExploreTutor from "../Props/ExploreTutor";
+
+const TutorProfileDetails = ({ tutorId, }) => {
   const [navfix, setNavfix] = useState(false);
   const [showCount, setShowCount] = useState(2);
 
@@ -51,9 +55,12 @@ const TutorProfileDetails = ({ tutorId }) => {
       }
     });
   };
+  const [loadmore, setLoadMore] = useState(4)
 
   const handleLoadMore = () => {
     setShowCount(ButtonData.length);
+    setLoadMore(prevNum => prevNum + 3);
+  
     // setSelectedSubjects(ButtonData.map(subject => subject.id));
   };
 
@@ -66,11 +73,13 @@ const TutorProfileDetails = ({ tutorId }) => {
   return (
     <div>
       <div
-        className={`z-10 ${
-          navfix ? "top-0 h-[4%] w-full fixed bg-[white]" : ""
+        className={`z-20  ${
+          navfix
+            ? "fixed top-0  shadow-lg w-full  bg-white transition-all duration-300 ease-in-out "
+            : ""
         }`}
       >
-        <Navbars className="relative" />
+        <Navbars className="relative " />
       </div>
 
       <section className="Contact_Us_Middle ">
@@ -80,7 +89,7 @@ const TutorProfileDetails = ({ tutorId }) => {
               <h3 className="text-2xl lg:text-4xl font-bold text-center text-cyan-700 py-5 ">
                 154,3 Search Result in “English” Tutor
               </h3>
-              <div className="grid grid-cols-12 gap-4  p-2 mt-10">
+              <div className="grid grid-cols-12 gap-5 lg:gap-12  p-2 mt-10">
                 <div className="col-span-12 md:col-span-8 lg:col-span-9 mb-6 lg:mb-0">
                   {B.map((TutorProfileProps) => (
                     //console.log(TutorProfileProps.length)
@@ -94,24 +103,24 @@ const TutorProfileDetails = ({ tutorId }) => {
                 <div className="col-span-12 md:col-span-4 lg:col-span-3 ">
                   <div>
                     <div className=" w-auto  bg-white border border-gray-200 rounded-lg shadow p-2 ">
-                      <h5 className="mb-3 p-2 text-base font-semibold text-black text-center  rounded-t-lg  border-b-2 w-full md:text-xl ">
-                        Educational Level
+                      <h5 className="mb-3 p-2 text-base font-semibold text-[#2c6777] text-center md:text-start rounded-t-lg  border-b-2 w-full md:text-xl ">
+                        I Can Teach
                       </h5>
 
                       <select
                         id="countries"
                         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
                       >
-                        <option>Choose a country</option>
+                        <option>Class (1-5)</option>
                         <option value="US">United States</option>
                         <option value="CA">Canada</option>
                         <option value="FR">France</option>
                         <option value="DE">Germany</option>
                       </select>
 
-                      <div>
-                        <h3 className="mb-4 font-semibold text-gray-900 dark:text-white">
-                          Identification
+                      <div className="p-1">
+                        <h3 className="mb-4 font-semibold text-[#2c6777] mt-1  ">
+                          Subject
                         </h3>
 
                         {ButtonData.slice(0, showCount).map((subject) => (
@@ -124,11 +133,11 @@ const TutorProfileDetails = ({ tutorId }) => {
                               type="checkbox"
                               checked={selectedSubjects.includes(subject.id)}
                               onChange={() => handleCheckboxChange(subject.id)}
-                              className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                              className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 "
                             />
                             <label
                               htmlFor={`default-checkbox-${subject.id}`}
-                              className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                              className="ml-2 text-sm font-medium text-gray-900 "
                             >
                               {subject.buttoncontant}
                             </label>
@@ -143,32 +152,116 @@ const TutorProfileDetails = ({ tutorId }) => {
                           </button>
                         )}
                       </div>
+                      <select
+                        id="countries"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                      >
+                        <option>Class(6-10)</option>
+                        <option value="US">United States</option>
+                        <option value="CA">Canada</option>
+                        <option value="FR">France</option>
+                        <option value="DE">Germany</option>
+                      </select>
+                      <select
+                        id="countries"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 mt-5"
+                      >
+                        <option>Short Course </option>
+                        <option value="US">United States</option>
+                        <option value="CA">Canada</option>
+                        <option value="FR">France</option>
+                        <option value="DE">Germany</option>
+                      </select>
+                      <select
+                        id="countries"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 mt-5 "
+                      >
+                        <option>Language Course</option>
+                        <option value="US">United States</option>
+                        <option value="CA">Canada</option>
+                        <option value="FR">France</option>
+                        <option value="DE">Germany</option>
+                      </select>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
 
-            <div>
-              <div className="flex justify-between items-center bg-[#2c6777] rounded-md p-5 mt-5 mb-5">
-                <div className="text-white">
-                  <h2 className="text-2xl font-semibold">
-                    Request the best tutor for your child
-                  </h2>
-                  <p>
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry.{" "}
-                  </p>
+           
+            <section className="">
+                <div className="container px-5 py-24 mx-auto">
+                    <div className="bg-[#2C6777] py-10 rounded-3xl ">
+                        <div className="lg:w-full flex items-center flex-col sm:flex-row text-center lg:text-start md:text-start mx-auto lg:px-10 px-4 ">
+                            <div className="lg:w-3/4 md:3/5">
+                                <h1 className="flex-grow sm:pr-16 text-2xl font-medium title-font text-white ">Create Tutor2u account now</h1>
+                                <p className="flex-grow sm:pr-16 text-xl font-medium title-font text-white">Join us and start your journey towards excellence.</p>
+                            </div>
+                            <div className="lg:w-1/4 md:2/5">
+                                <button className="flex-shrink-0 text-[#21515e] font-semibold bg-white border-0 py-2 lg:px-8 md:px-5 px-8  hover:bg-[#5cdcff]  rounded-3xl text-lg mt-10 sm:mt-0">Request a Tutor</button>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+                    
+
                 </div>
-                <div>
-                  <button className="bg-white text-black text-sm font-normal px-3 py-2 rounded-full">
-                    Request a Tutor
-                  </button>
-                </div>
-              </div>
-            </div>
+
+            </section>
+
           </div>
+          
         </div>
+      </section>
+      
+      <section className="div3-Section w-full bg-[#dcecfa] " >
+        <div className=" mt-25   container mx-auto">
+
+
+
+
+          <div className=" p-5 md:p-0 ">
+
+            <h1 className="text-[24px] text-[#2c6777] xl:text-[30px] lg:text-[30px] md:text-[27px] text-center py-16  font-[650] max-w-xl mx-auto ">Explore Related Tutors</h1>
+          </div>
+
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5  p-2 md:p-0 justify-items-center     ">
+
+
+            {
+              ExploreTutordata.slice(0, loadmore).map((exploretutordata) => (
+
+                <ExploreTutor key={exploretutordata.id} exploretutordata={exploretutordata}></ExploreTutor>
+
+              ))
+            }
+
+          </div>
+          <div className="text-center lg:mt-2 ">
+
+            {ExploreTutordata.length > loadmore && (
+              <button
+                onClick={handleLoadMore}
+                className="bg-[#2c6777] hover:bg-[#2c6777]  mb-10  text-white font-bold py-2 px-9 rounded-md "
+              >
+                Show More
+              </button>
+            )}
+
+          </div>
+
+
+
+
+        </div>
+
+
+
+      </section>
+      <section className="">
+        <Footer />
       </section>
     </div>
   );
